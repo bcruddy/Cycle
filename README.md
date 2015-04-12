1ee9ac1a5891f9bfa3281bba0136237b1e5119fa
ruddy <brian@photofy.com>
2015-04-12 18:35:41 -0400

    inject dynamic stylesheet into page, add readme, options via data-attributes

--- README.md
+++ README.md
@@ -2,16 +2,31 @@
 
 imageCycle is a vanilla javascript plugin intended to cycle through images but can be used for pretty much anything.
 
+### Usage
+
+If using more than one instance of imageCycle, each requires a unique selector. If using two instances on a single page, they need to be instantiated twice, eg:
+
+    <script src="/path/to/image-cycle.js"></script>
+    <script>
+        imageCycle('.firstInstance');
+        imageCycle('.secondInstace');
+    <script>
+
+Selector defaults to `.image-cycle` if one is not given.
+
 ### Options
 
-imageCycle's options are controlled via data attributes on your target element. Current options are:
+imageCycle's options are controlled via data attributes on your target element. Available options are:
 
-- target: elements targeted by imageCycle
+- target: elements targeted by imageCycle (default: "li")
 
-- interval: interval between changes in ms
+- interval: interval between changes in ms (default: "2500"
 
-- width: max width of the imageCycle container
+- width: max width of the imageCycle container (default: "300")
+
+- speed: speed of transition between elements (default: "1000")
 
 ### Example
 
 Clone/fork the repo and you the example directory is good to go.
+
--- src/image-cycle.js
+++ src/image-cycle.js
@@ -1,23 +1,38 @@
 /**
  * Created by <brian at briancruddy dot com> on 4/12/15.
  */
-(function () {
+
+var imageCycle = function (selector) {
 
     'use strict';
 
-    var selector = '.image-cycle',
-        target, interval, images, width;
-
-    function sel(selector) {
+    function sel (selector) {
         return document.querySelector(selector);
     }
 
-    width = sel(selector).dataset.width;
-    sel(selector).style.width = width;
+    if (!selector) { selector = '.image-cycle'; }
 
-    target = sel(selector).dataset.target;
-    interval = sel(selector).dataset.interval;
+    var target, interval, width, speed, images, styleSheet;
+
+    target = sel(selector).dataset.target || 'li';
+    interval = sel(selector).dataset.interval || '2500';
+    width = sel(selector).dataset.width || '300';
+    speed = sel(selector).dataset.speed || '1000';
     images = document.querySelectorAll(selector + ' ' + target);
+    styleSheet = (function() {
+        var style = document.createElement('style');
+
+        // webkit hack
+        style.appendChild(document.createTextNode(''));
+        document.head.appendChild(style);
+
+        return style.sheet;
+    })();
+
+    styleSheet.insertRule(selector + ' { max-width: 100%; position: relative; width: ' + width + 'px; }', 0);
+    styleSheet.insertRule(selector + ' > * { position: absolute; top: 0; left: 0; z-index: 0; opacity: 0; transition: opacity 300ms }', 0);
+    styleSheet.insertRule(selector + ' > .active { z-index: 1; opacity: 1; transition: opacity ' + speed + 'ms; }', 0);
+    styleSheet.insertRule(selector + ' img { width: 100%; }', 0);
 
     var i = 1; // first image is already active, start with second
     setInterval(function () {
@@ -31,5 +46,5 @@
         i++;
     }, interval);
 
-})();
+};
 
