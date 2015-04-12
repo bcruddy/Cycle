a22f18bdeec19349288fca78ec3f546580ceb9a3
ruddy <brian@photofy.com>
2015-04-12 17:46:34 -0400

    simplified and added css transitions

--- src/image-cycle.js
+++ src/image-cycle.js
@@ -1,44 +1,35 @@
 /**
  * Created by <brian at briancruddy dot com> on 4/12/15.
  */
-var imageCycle = (function (options) {
+(function () {
 
     'use strict';
 
-    var target, interval, images;
+    var selector = '.image-cycle',
+        target, interval, images, width;
 
-    if (!options) {
-        options = {
-            selector: '.image-cycle',
-            target: 'li',
-            interval: 2000
-        };
+    function sel(selector) {
+        return document.querySelector(selector);
     }
 
-    target = document.querySelector(options.selector).dataset.target;
-    interval = document.querySelector(options.selector).dataset.interval;
+    width = sel(selector).dataset.width;
+    sel(selector).style.width = width;
 
-    if (target) {
-        options.target = target;
-    }
+    target = sel(selector).dataset.target;
+    interval = sel(selector).dataset.interval;
+    images = document.querySelectorAll(selector + ' ' + target);
 
-    if (interval) {
-        options.interval = interval;
-    }
-
-    images = document.querySelectorAll(options.selector + ' ' + options.target);
-
-    var i = 0;
+    var i = 1; // first image is already active, start with second
     setInterval(function () {
         if (i == images.length) { i = 0; }
 
         for (var j = 0; j < images.length; j++) {
-            images[j].style.zIndex = 0;
+            images[j].classList.remove('active');
         }
 
-        images[i].style.zIndex = 1;
+        images[i].classList.add('active');
         i++;
-    }, options.interval);
+    }, interval);
 
-});
+})();
 
