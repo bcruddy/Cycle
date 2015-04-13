38324ee42eefc364e43ce09f7a64d5a1c0a74040
ruddy <brian@photofy.com>
2015-04-12 23:56:04 -0400

    create and modify Cycle object

--- README.md
+++ README.md
@@ -4,13 +4,35 @@ imageCycle is a vanilla javascript plugin intended to cycle through images but c
 
 ### Usage
 
-If using more than one instance of imageCycle, each requires a unique selector. If using two instances on a single page, they need to be instantiated twice, eg:
+    <h2>Image Cycle Demo</h2>
+    <div class="wrapper">
+        <div class="half">
+            <ul class="image-cycle" data-target="li" data-interval="2500" data-width="300" data-speed="1000">
+                <li class="active" ><img src="images/ex1.jpg"/></li>
+                <li><img src="images/ex2.jpg" /></li>
+                <li><img src="images/ex3.jpg" /></li>
+            </ul>
+        </div>
 
-    <script src="/path/to/image-cycle.js"></script>
+        <div class="half">
+            <ul class="image-cycle2" data-target="li" data-interval="3500" data-width="320" data-speed="2000">
+                <li class="active" ><img src="images/ex1.jpg"/></li>
+                <li><img src="images/ex2.jpg" /></li>
+                <li><img src="images/ex3.jpg" /></li>
+            </ul>
+        </div>
+    </div>
+        
+    <script src="/src/image-cycle.js"></script>
     <script>
-        imageCycle('.firstInstance');
-        imageCycle('.secondInstace');
-    <script>
+        var ex1 = new Cycle('.image-cycle');
+        ex1.set('interval', '2000'); // overrides 2500 set in data-attribute
+        console.log(ex1.get('speed')); // returns and logs "2000"
+    
+        var ex2 = new Cycle('.image-cycle2');
+    </script>
+
+If using more than one instance of imageCycle, each requires a unique selector. If using two instances on a single page, they need to be instantiated twice, eg:
 
 Selector defaults to `.image-cycle` if one is not given.
 
@@ -22,13 +44,19 @@ See `example/index.html` for a usage example.
 
 imageCycle's options are controlled via data attributes on your target element. Available options are:
 
-- target: elements targeted by imageCycle (default: "li")
+- target: elements targeted by imageCycle (default: `"li"`)
 
-- interval: interval between changes in ms (default: "2500"
+- interval: interval between changes in ms (default: `"2500"`)
 
-- width: max width of the imageCycle container (default: "300")
+- width: max width of the imageCycle container (default: `"300"`)
 
-- speed: speed of transition between elements (default: "1000")
+- speed: speed of transition between elements (default: `"1000"`)
+
+### Methods
+
+`.get(property)` retrieves a property's value
+
+`.set(property, value)` sets a new value and re-instantiates the plugin.
 
 ### Example
 
--- src/image-cycle.js
+++ src/image-cycle.js
@@ -2,24 +2,45 @@
  * Created by <brian at briancruddy dot com> on 4/12/15.
  */
 
-var imageCycle = function (selector) {
+function sel (selector) {
+    return document.querySelector(selector);
+}
 
-    'use strict';
+function Cycle (selector) {
 
-    function sel (selector) {
-        return document.querySelector(selector);
-    }
+    this.selector = selector || '.image-cycle';
+    this.target = sel(selector).dataset.target || 'li';
+    this.interval = sel(selector).dataset.interval || '2500';
+    this.width = sel(selector).dataset.width || '300';
+    this.speed = sel(selector).dataset.speed || '1000';
+    this.images = document.querySelectorAll(selector + ' ' + this.target);
 
-    if (!selector) { selector = '.image-cycle'; }
+    this.style();
+    this.init();
 
-    var target, interval, width, speed, images, styleSheet;
+    return this;
+}
 
-    target = sel(selector).dataset.target || 'li';
-    interval = sel(selector).dataset.interval || '2500';
-    width = sel(selector).dataset.width || '300';
-    speed = sel(selector).dataset.speed || '1000';
-    images = document.querySelectorAll(selector + ' ' + target);
-    styleSheet = (function() {
+
+Cycle.prototype.init = function () {
+    var _this = this,
+        i = 1; // first image is already active, start with second
+    setInterval(function () {
+        if (i === _this.images.length) { i = 0; }
+
+        for (var j = 0; j < _this.images.length; j++) {
+            _this.images[j].classList.remove('active');
+        }
+
+        _this.images[i].classList.add('active');
+        i++;
+    }, this.interval);
+};
+
+
+Cycle.prototype.style = function () {
+
+    var styleSheet = (function() {
         var style = document.createElement('style');
 
         // webkit hack
@@ -29,22 +50,21 @@ var imageCycle = function (selector) {
         return style.sheet;
     })();
 
-    styleSheet.insertRule(selector + ' { max-width: 100%; position: relative; width: ' + width + 'px; }', 0);
-    styleSheet.insertRule(selector + ' > * { position: absolute; top: 0; left: 0; z-index: 0; opacity: 0; transition: opacity 300ms }', 0);
-    styleSheet.insertRule(selector + ' > .active { z-index: 1; opacity: 1; transition: opacity ' + speed + 'ms; }', 0);
-    styleSheet.insertRule(selector + ' img { width: 100%; }', 0);
-
-    var i = 1; // first image is already active, start with second
-    setInterval(function () {
-        if (i == images.length) { i = 0; }
-
-        for (var j = 0; j < images.length; j++) {
-            images[j].classList.remove('active');
-        }
-
-        images[i].classList.add('active');
-        i++;
-    }, interval);
-
+    styleSheet.insertRule(this.selector + ' { max-width: 100%; position: relative; width: ' + this.width + 'px; }', 0);
+    styleSheet.insertRule(this.selector + ' > * { position: absolute; top: 0; left: 0; z-index: 0; opacity: 0; transition: opacity 300ms }', 0);
+    styleSheet.insertRule(this.selector + ' > .active { z-index: 1; opacity: 1; transition: opacity ' + this.speed + 'ms; }', 0);
+    styleSheet.insertRule(this.selector + ' img { width: 100%; }', 0);
 };
+
+
+Cycle.prototype.set = function (property, value) {
+    this[property] = value;
+    this.init();
+};
+
+
+Cycle.prototype.get = function (property) {
+    return this[property];
+};
+
 
